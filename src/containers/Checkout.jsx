793ef5a1781a219bfa6502a2e743ec658f881b2f
David Gonzalez <davidgonzalez0519@gmail.com>
2021-03-27 17:04:56 -0500

    Bug pending in Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -7,26 +7,40 @@ const Checkout = () => {
   const { state, removeFromCart } = useContext(AppContext);
   const { cart } = state;
 
+  const handleRemove = (product) => () => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (acumulator, currentValue) =>
+      acumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
-    <div>
-      <div className="Checkout-content">
-        <h3>Lista de Pedidos</h3>
+    <div className="Checkout-content">
+      {cart.length > 0 ? <h3>Lista de Pedidos</h3> : <h3>Sin Pedidos ...</h3>}
+      {/*Hay un bug en el map que no permite visualizar la lista de pedidos */}
+      {cart.map((item) => {
         <div className="Checkout-item">
           <div className="Checkout-element">
-            <h4>Item name</h4>
-            <span>$10</span>
+            <h4>{item.title}</h4>
+            <span>${item.price}</span>
           </div>
-          <button type="button">
+          <button type="button" onClick={handleRemove(item)}>
             <i className="fas fa-times-circle" />
           </button>
-        </div>
+        </div>;
+      })}
+      {cart.length > 0 && (
         <div className="Checkout-sidebar">
-          <h3>Precio Total: $10</h3>
+          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
           <Link to="/checkout/information">
             <button type="button">Continuar Pedido</button>
           </Link>
         </div>
-      </div>
+      )}
     </div>
   );
 };
