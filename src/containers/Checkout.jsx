a478afaac1478a9b1052c2c998a66045ae60ce54
David Gonzalez <davidgonzalez0519@gmail.com>
2021-04-07 09:25:26 -0500

    estruct app finished

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -22,8 +22,8 @@ const Checkout = () => {
     <div className="Checkout-content">
       {cart.length > 0 ? <h3>Lista de Pedidos</h3> : <h3>Sin Pedidos ...</h3>}
       {/*Hay un bug en el map que no permite visualizar la lista de pedidos */}
-      {cart.map((item) => {
-        <div className="Checkout-item">
+      {cart.map((item) => (
+        <div className="Checkout-item" key={item.id}>
           <div className="Checkout-element">
             <h4>{item.title}</h4>
             <span>${item.price}</span>
@@ -31,8 +31,8 @@ const Checkout = () => {
           <button type="button" onClick={handleRemove(item)}>
             <i className="fas fa-times-circle" />
           </button>
-        </div>;
-      })}
+        </div>
+      ))}
       {cart.length > 0 && (
         <div className="Checkout-sidebar">
           <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
 const Information = () => {
+  const { state, addToBuyer } = useContext(AppContext);
+  const form = useRef(null);
+  const history = useHistory();
+
+  const { cart } = state;
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const buyer = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      address: formData.get('address'),
+      apto: formData.get('apto'),
+      city: formData.get('city'),
+      country: formData.get('country'),
+      cp: formData.get('cp'),
+      phone: formData.get('phone'),
+    };
+    addToBuyer(buyer);
+    history.push('/checkout/payment');
+  };
+
   return (
     <div className="Information">
       <div className="Information-content">
@@ -10,7 +34,7 @@ const Information = () => {
           <h2>Informacion de contacto: </h2>
         </div>
         <div className="Information-form">
-          <form action="">
+          <form ref={form}>
             <input type="text" placeholder="Nombre Completo" name="name" />
             <input type="text" placeholder="Correo Electonico" name="email" />
             <input type="text" placeholder="Direccion" name="address" />
@@ -22,20 +46,28 @@ const Information = () => {
           </form>
         </div>
         <div className="Information-buttons">
-          <div className="Information-back">Regresar</div>
+          <div className="Information-back">
+            <Link to="/checkout">Regresar</Link>
+          </div>
         </div>
         <Link to="/checkout/payment">
-          <div className="Information-next">Pagar</div>
+          <div className="Information-next">
+            <button type="button" onClick={handleSubmit}>
+              Pagar
+            </button>
+          </div>
         </Link>
       </div>
       <div className="Information-sidebar">
         <h3>Pedido:</h3>
-        <div className="Information-item">
-          <div className="Information-element">
-            <h4>ITEM Name</h4>
-            <span>$10</span>
+        {cart.map((item) => (
+          <div className="Information-item" key={item.id}>
+            <div className="Information-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
